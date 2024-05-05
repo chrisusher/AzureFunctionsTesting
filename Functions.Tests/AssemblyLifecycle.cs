63ab699a6462f6bba4a8f5e0df362f3bc6ed73ec
Chris Usher <chrisushertester@gmail.com>
2024-05-05 16:31:31 +0100

    Initial Project Setup and CI Workflow

---
+++ Functions.Tests/AssemblyLifecycle.cs
@@ -0,0 +1,19 @@
+﻿using DockerBuilder = Ductus.FluentDocker.Builders.Builder;
+
+namespace Functions.Tests;
+
+[SetUpFixture]
+public class AssemblyLifecycle
+{
+    [OneTimeSetUp]
+    public async Task AssemblySetupAsync()
+    {
+        
+    }
+
+    [OneTimeTearDown]
+    public async Task AssemblyTeardownAsync()
+    {
+        
+    }
+}
---
+++ Functions.Tests/TestCommon.cs
@@ -0,0 +1,6 @@
+﻿namespace Functions.Tests;
+
+public static class TestCommon
+{
+    
+}
